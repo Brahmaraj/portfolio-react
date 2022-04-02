6ab446850b600e8b40e9bdd8745bdd14aeb699c4
Brahmaraj <brahmarajrathod@gmail.com>
2022-04-03 00:12:47 +0530

    added icons and contact form updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+import mail from "../Assets/mail.png";
+import location from "../Assets/location.jpg";
+import phone from "../Assets/phone.jpg";
 
 const Contact = () => {
     return (
@@ -9,9 +12,18 @@ const Contact = () => {
                     <h2 className="color_green contact_form_title">Let's connect</h2>
                     <br />
                     <br />
-                    <p>let's connect, always up for a good conversation and can share our journey and bad decisions without judging xD.</p>
+                    <p className="contact_para">let's connect, always up for a good conversation and can share our journey and bad decisions without judging xD.</p>
                     <br />
-                    <p>I love talking about New Technology, Cyber Security and Web 3.0 related stuff. Interested in Stock Market, Crypto Currency, Fitness, Fashion and Adventures</p>
+                    <p className="contact_para">I love talking about New Technology, Cyber Security and Web 3.0 related stuff. Interested in Stock Market, Crypto Currency, Fitness, Fashion and Adventures</p>
+                    <br />
+                    <br />
+                    <div className="contact_grey_text" style={{display:"grid",gridTemplateColumns:"40px 300px",alignItems:"center"}}>
+                        <img src={mail} alt="mail" width="25px"/> brahmarajrathod@gmail.com
+                        <br />
+                        <img src={phone} alt="phone" width="25px"/> 8108210821
+                        <br />
+                        <img src={location} alt="location" width="25px"/> Mulund East, Mumbai - 400081.
+                    </div>
                 </div>
                 <div className="contact_form">
                     <h2 className="color_green contact_form_title">Send me a message</h2>
